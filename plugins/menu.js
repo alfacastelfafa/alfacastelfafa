340005342d3d49f309122823a35819c7e7a68d93
Alfacastel <alfacastel3@gmail.com>
2022-10-16 18:57:05 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -276,7 +276,7 @@ const sections = [
         {title: `⚡ ${pmenus} SPEED BOT`, rowId: ".speed", description: "Menampilkan kecepatan respon BOT"},
         {title: `💌 ${pmenus} OWNER BOT`, rowId: ".owner", description: "Menampilkan List owner BOT"},
         {title: `⏰ ${pmenus} RUNTIME BOT`, rowId: ".runtime", description: "𝙼𝚎𝚗𝚊𝚖𝚙𝚒𝚕𝚔𝚊𝚗 Waktu Bot Berjalan"}, 
-        {title: `📔 ${pmenus} SCRIPT BOT`, rowId: ".sc", description: `Source Code ${namebot}`},
+        {title: `💼 ${pmenus} LIST SHOP`, rowId: ".shop", description: `Shop By ${namebot}`},
     ]
       },{
         title: `${htki} SUPPORT ${htka}`,
